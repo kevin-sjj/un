9ec334ba33459262b07cdb24ea1e9b979be5b66b
kevin.sjj <kevin.sjj@kakaoent.com>
2024-04-29 18:39:16 +0900

    ddd

--- src/components/ButtonA.tsx
+++ src/components/ButtonA.tsx
@@ -3,3 +3,5 @@
 export default function ButtonA() {
   return <button className="py-4 bg-gray-500 w-full block" onClick={() => {console.log('on'); console.log('click'); console.log('button A')}}>Button A</button>
 }
+
+//123
